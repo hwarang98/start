68987845510da53e0b92e1b98eb4b1a3013d27eb
coderang <84739055+hwarang98@users.noreply.github.com>
2021-05-25 22:08:28 +0900

    자바 문자열
    생활코딩의 유튜브 강좌 JAVA 강좌 중 문자 열 에 대한것을 공부했다.

---
+++ StringApp.java
@@ -0,0 +1,16 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello world"); //String
+		System.out.println('h'); // Character
+		System.out.println("h"); 
+		
+		System.out.println("hello \nworld");
+		System.out.println("hello \"world\"");
+ 
+
+	}
+
+}
